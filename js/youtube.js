a5debb9c85301cc7b193222a5e5f4e9d1d1c8ac6
nishilshah17 <nishil.shah17@gmail.com>
2014-05-25 17:03:22 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -392,16 +392,16 @@ function getData(){
 	                url: 'http://clubbedinapp.com/ypr/getremoved.php',
 	                crossDomain: true,
 	                type: 'post',
+                    async: false,
 	                success: function (data) {
-                        alert(data);
                         var json = jQuery.parseJSON(data);
                         for(var i=0; i<json.length; i++)
                         {
-                            alert(json[i]);
                             removedVideos.push(json[i]);
                         }
 	                }
 	            });
+    
     alert(removedVideos);
 }
 
