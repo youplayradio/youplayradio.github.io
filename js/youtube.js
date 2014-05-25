84212946000b798039d2771e1287c69a394ba0a2
nishilshah17 <nishil.shah17@gmail.com>
2014-05-25 17:05:51 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -39,10 +39,12 @@ function searchVideosMoods() {
 }
 
 function parseResultsMoods() {
-    
+    alert(removedVideos);
     $.each(results.items, function(item) {
-        if(results.items[item].id.videoId != null && results.items[item].id.videoId != undefined)
+        if(results.items[item].id.videoId != null && results.items[item].id.videoId != undefined){
+            if(removedVideos.indexOf(results.items[item].id.videoID == -1)
                 videoIDs.push(results.items[item].id.videoId);
+        }
     });
     
     var nextPageToken = results.nextPageToken;
