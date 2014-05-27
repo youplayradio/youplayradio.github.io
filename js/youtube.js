43b8943c1b8e9b0417c7d2231bc6bbf61e2c2fc1
nishilshah17 <nishil.shah17@gmail.com>
2014-05-27 17:33:35 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -283,6 +283,7 @@ function onYouTubeIframeAPIReady() {
 function onSliderChange(sliderVal) {
     currentVolume = sliderVal;
     player.setVolume(currentVolume);
+    alert(player);
 }
 
 function setSlider() {
