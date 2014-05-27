255dd71ddb9dd29040b73fafb6c2401e0bb5c6e7
Rohfosho <rohvar98@gmail.com>
2014-05-26 23:25:29 -0400

    Revert "fixed vulner"
    This reverts commit e5f814245d8a516e19738f233a067c35cd72bd5d.

---
+++ ypr/getremoved.php
@@ -0,0 +1,23 @@
+<?php
+header("access-control-allow-origin: *");
+
+$dbhost = 'localhost';
+$dbuser = 'clubbed_admin';
+$dbpass = 'ClubbedIn2013';
+$db = 'clubbed_ypr';
+
+$dbserver = mysql_connect($dbhost, $dbuser, $dbpass)
+    or die("Unable to connect to MySQL: " . mysql_error());
+mysql_select_db($db)
+    or die("Unable to select database: " . mysql_error());
+
+$result = mysql_query('SELECT * FROM removed');
+$removed = array();
+
+while ($row = mysql_fetch_assoc($result))
+{
+    array_push($removed, $row['videoID']);
+}
+
+echo json_encode($removed);
+?>
